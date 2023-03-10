a81a77ed9a36496392c1bc5c194cdff4dcd9ffad
lobbs <lobbs@lobbss-MacBook-Air-2.local>
2023-03-09 19:40:49 -0600

    updated - added prompt/loop

--- practice.js
+++ practice.js
@@ -112,3 +112,29 @@ if(a == 5 && b == 6) {
  for (i of names) {
     console.log(i);
  }
+
+ //way to get user input - not for advanced coding(beginnner level)
+//var name = window.prompt('What is your name');
+//window.alert(`Welcome, ${name}`);
+
+//let username = prompt('Username:');
+//let password = prompt('Password:');
+
+//if(username == 'samy123' && password == '12345') {
+   // alert('Welcome back,' + username);
+//} else {
+    //alert('Incorrect username or password');
+//}
+
+var loggedIn = false;
+
+while(!loggedIn) {
+    let username = prompt('Username:');
+    let password = prompt('Password:');
+    if(username == 'samy123' && password == '12345') {
+        alert('Welcome back,' + username);
+        loggedIn = true;
+    } else {
+        alert('Incorrect username or password');
+    }
+}
